cd54998ed332e1f8bf5ac79bbd0f4df51d8325d2
Ryan Garland <ryangarland211@gmail.com>
2022-10-19 13:05:16 -0400

    leaderboard should work now

--- common.php
+++ common.php
@@ -7,13 +7,62 @@
 </head>
 
 <body>
-   
+   <table>
+    <tr>
+        <td>Place</td>
+        <td>Name</td>
+        <td>Score</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+   </table>
 
     <?php
     function leaderboad(){
-        
+        $myfile = fopen("./leaderboard.txt", "r") or die("Unable to open file!");
+        $i = 1;
+        echo "
+        <table>
+    <tr>
+        <td>Place</td>
+        <td>Name</td>
+        <td>Score</td>
+    </tr>";
+    while($line = fgets($myfile)){
+        $curr = explode(",",$line);
+    // the leaderboard.txt file should be [score, name] because sorting is easier now
+    echo "<tr>
+    
+        <td>$i</td>
+        <td>$curr[1]</td>
+        <td>$curr[0]</td>
+    </tr>";
+    $i++;
     }
-     
+   echo "</table>";
+   
+
+    }
+
+    function sortLeaders(){
+        $file_path = './leaderboard.txt';
+        $new_file_path = './leaderboard.txt';
+
+        $data = file($file_path);
+        natsort($data);
+        file_put_contents($new_file_path, implode("\n", $data));
+    }
+    
+    function insertLeader($name,$score){
+    $myfile = fopen("leaderboard.txt", "w") or die("Unable to open file!");
+    $txt = $name.','.$score.'\n';
+    fwrite($myfile, $txt);
+
+    fclose($myfile);
+    }
     
     ?>
         
