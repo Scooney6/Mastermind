76b0aba8a625729eff5e21e7ebd09a0f3dd8aaf0
Scooney6 <66892347+Scooney6@users.noreply.github.com>
2022-11-01 16:37:25 -0400

    Return easy difficulty to random state

--- game-handler.php
+++ game-handler.php
@@ -6,8 +6,7 @@ function generateCode($Difficulty) {
     global $colors;
     $colors = array("red", "yellow", "green", "blue", "black", "purple", "brown");
     if($Difficulty == "Easy") {
-        //return(array($colors[rand(0, 4)], $colors[rand(0, 4)], $colors[rand(0, 4)], $colors[rand(0, 4)]));
-        return(array("blue", "blue", "green", "green"));
+        return(array($colors[rand(0, 4)], $colors[rand(0, 4)], $colors[rand(0, 4)], $colors[rand(0, 4)]));
     }
     elseif($Difficulty == "Medium") {
         return(array($colors[rand(0, 5)], $colors[rand(0, 5)], $colors[rand(0, 5)], $colors[rand(0, 5)]));
